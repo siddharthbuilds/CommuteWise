d79a69328a7193cd771680c39213bb49eac7e788
Adhitya R <adhityarangaraj@gmail.com>
2026-06-15 01:56:16 +0530

    Merged the two datasets

--- ml-models/Trail Dataset/data_merging.py
+++ ml-models/Trail Dataset/data_merging.py
@@ -2,3 +2,12 @@ import pandas as pd
 from data_cleaning import df
 from weather_dataset import weather_df
 df['DATE'] = pd.to_datetime(df['DATE'])
+weather_df['DATE']=pd.to_datetime(weather_df['DATE'])
+df=pd.merge(df,weather_df,on='DATE',how='inner')
+print(df.head(10))
+print(df.columns)
+print(df.shape)
+df['weekday'] = df['DATE'].dt.strftime('%a')
+print(df.head(10))
+print(df.columns)
+print(df.shape)
