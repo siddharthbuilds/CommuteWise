ef3f51d25e258e37a8d5ade4e1bb32345baafc3a
Siddharth Krishna <siddharthtnj2020@gmail.com>
2026-06-15 13:07:22 +0530

    added weights and normalised values to get_rating

--- backend/routes/routes.py
+++ backend/routes/routes.py
@@ -14,11 +14,18 @@ class Routes:
     def get_carbonrate(self):
         self.carbonrate=0
 
-    def get_rating(self):
-        self.marks=0
+    def get_rating(self,max_values):
+        self.rating=0
+        current_values=[self.distance,self.duration,self.expenditure,self.timedelay,self.carbonrate]
+        rating_weights=[0.20,0.35,0.25,0.05,0.15]
+        i=0
+        while(i<5):
+            normalised_value=((1-(current_values[i]/max_values[i]))*100)
+            self.rating+=normalised_value*rating_weights[i]
+            i+=1
 
     def add_badge(self,str):
-        self.badge.append(str)
+        self.badges.append(str)
     
     def __init__(self,mode):
         self.mode=mode
@@ -28,4 +35,3 @@ class Routes:
         self.get_expenditure()
         self.get_timedelay()
         self.get_carbonrate()
-        self.get_rating()
