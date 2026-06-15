bae347ff41d9bf61aeecf517982c385edac9b38f
Siddharth Krishna <siddharthtnj2020@gmail.com>
2026-06-15 12:34:39 +0530

    added get_all_routes and badges functions

--- backend/main.py
+++ backend/main.py
@@ -37,15 +37,26 @@ class Routes:
 
     def get_rating(self):
         self.marks=0
+
+    def add_badge(self,str):
+        self.badge.append(str)
     
     def __init__(self,mode):
         self.mode=mode
+        self.badges=[]
         self.get_distance()
         self.get_duration()
         self.get_expenditure()
         self.get_timedelay()
         self.get_carbonrate()
         self.get_rating()
+
+def get_all_routes(source,destination):
+    all_routes=[]
+    return all_routes
+
+def get_badges(all_routes):
+    return
 
 @app.get('/api/home')
 def home():
