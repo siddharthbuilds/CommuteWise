aebfab84b2ee373f47ee9082cadfd7545872dde3
Siddharth Krishna <siddharthtnj2020@gmail.com>
2026-06-15 14:36:46 +0530

    added get final data function

--- backend/routes/route_functions.py
+++ backend/routes/route_functions.py
@@ -1,23 +1,23 @@
-def get_all_routes(source,destination):
+def get_all_routes(source,destination,date,time):
     all_routes=[]
     return all_routes
 
 def get_max_values(all_routes):
+    max_distance = max(r.distance for r in all_routes)
     max_duration = max(r.duration for r in all_routes)
     max_expenditure = max(r.expenditure for r in all_routes)
-    max_delay = max(r.timedelay for r in all_routes)
-    max_carbon = max(r.carbonrate for r in all_routes)
-    max_distance = max(r.distance for r in all_routes)
+    max_timedelay = max(r.timedelay for r in all_routes)
+    max_carbonrate = max(r.carbonrate for r in all_routes)
 
-    max_values = [max_distance,max_duration,max_expenditure,max_delay,max_carbon]
+    max_values = [max_distance,max_duration,max_expenditure,max_timedelay,max_carbonrate]
     for routes in all_routes:
         routes.get_rating(max_values)
 
 def get_min_values(all_routes):
     min_duration = min(r.duration for r in all_routes)
     min_expenditure = min(r.expenditure for r in all_routes)
-    min_carbon = min(r.carbonrate for r in all_routes)
-    return [min_expenditure,min_duration,min_carbon]
+    min_carbonrate = min(r.carbonrate for r in all_routes)
+    return [min_expenditure,min_duration,min_carbonrate]
 
 def get_badges(all_routes):
     min_values=get_min_values(all_routes)
@@ -30,3 +30,20 @@ def get_badges(all_routes):
             routes.add_badge("fastest")
         if routes.expenditure==min_values[2]:
             routes.add_badge("safest")
+    return sorted_all_routes
+
+def get_final_data(sorted_all_routes):
+    final_routes=list()
+    for routes in sorted_all_routes:
+        route_dict={
+                    "mode":routes.mode,
+                    "distance": routes.distance,
+                    "duration":routes.duration,
+                    "expenditure":routes.expenditure,
+                    "timedelay":routes.timedelay,
+                    "carbonrate":routes.carbonrate,
+                    "rating":routes.rating,
+                    "badges":routes.badges   
+                    }
+        final_routes.append(route_dict)
+    return final_routes
