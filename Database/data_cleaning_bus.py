621490eb9c8f079612158e256bad6b5b44a548a0
Adhitya R <adhityarangaraj@gmail.com>
2026-06-15 14:52:00 +0530

    Added bus graph to main graph

--- Database/data_cleaning_bus.py
+++ Database/data_cleaning_bus.py
@@ -1,46 +1,23 @@
 import pandas as pd
 from bfs_algo import bfs
+from collections import defaultdict
 df_bus=pd.read_csv("route_detail (1).csv")
 print(df_bus.columns.tolist())
 print(df_bus.shape)
-
-
 route = "1"
-
 print(
     df_bus[df_bus["route_id"] == route]
 )
 print(df_bus["route_id"].nunique())
 print(df_bus["stop_name"].nunique())
-from collections import defaultdict
-
 bus_graph = defaultdict(set)
-
-# ensure stop order
 df_bus["stop_id"] = df_bus["stop_id"].astype(int)
 
 for route_id, group in df_bus.groupby("route_id"):
-
     group = group.sort_values("stop_id")
-
     stops = group["stop_name"].str.strip().tolist()
-
     for i in range(len(stops) - 1):
-
         current = stops[i]
         next_stop = stops[i + 1]
-
         bus_graph[current].add(next_stop)
         bus_graph[next_stop].add(current)
-print(len(bus_graph))
-
-print(bus_graph["THIRUVOTRIYUR"])
-
-print(bus_graph["PARRYS"])
-path = bfs(
-    bus_graph,
-    "THIRUVOTRIYUR",
-    "THIRUVANMIYUR"
-)
-
-print(path)
--- Database/graph_finder.py
+++ Database/graph_finder.py
@@ -1,6 +1,7 @@
 from data_cleaning_metro import df
 from data_cleaning_electric_train import df_rail
 from collections import defaultdict
+from data_cleaning_bus import bus_graph
 graph = defaultdict(set)
 for corridor, group in df.groupby("Corridor Name"):
     stations = group["Station Name"].tolist()
@@ -26,3 +27,7 @@ transfer_map = {
 for rail, metro in transfer_map.items():
     graph[rail].add(metro)
     graph[metro].add(rail)
+
+for stop, neighbours in bus_graph.items():
+    for neighbour in neighbours:
+        graph[stop].append(neighbour)
