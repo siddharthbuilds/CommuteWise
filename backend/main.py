7483d18467d790765b0b4f985337bd4fb009a521
Siddharth Krishna <siddharthtnj2020@gmail.com>
2026-06-15 11:59:17 +0530

    created getroute and home routes

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
+from pydantic import BaseModel
 
 GOOGLE_API_KEY = ''
 app = FastAPI(title='CommuteWise')
@@ -12,9 +13,20 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get('/')
+class RouteRequest (BaseModel):
+    source:str
+    destination:str
+    date:str
+    time:str
+
+@app.get('/api/home')
 def home():
-    return "Hello"
+    return {"message": "Backend API works!!"}
+
+@app.post('/api/getroutes')
+def get_routes(data:RouteRequest):
+    return {data.source}
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app",host="127.0.0.1", port=8000, reload=True)
