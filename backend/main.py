192c88b34e44397f0f62da214861bc5328c7e079
Siddharth Krishna <siddharthtnj2020@gmail.com>
2026-06-16 06:56:22 +0530

    add get-all-routes to main

--- backend/main.py
+++ backend/main.py
@@ -3,9 +3,8 @@ from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from pydantic import BaseModel
 from routes.routes import Routes
-from routes.route_functions import get_all_routes, get_badges
+from routes.route_functions import get_all_routes
 
-GOOGLE_API_KEY = ''
 app = FastAPI(title='CommuteWise')
 app.add_middleware(
     CORSMiddleware,
@@ -27,7 +26,8 @@ def home():
 
 @app.post('/api/routes')
 def routes(data:RouteRequest):
-    return {data.source}
+    final_routes = get_all_routes(data.source,data.destination)
+    return {"routes": final_routes}
 
 if __name__ == "__main__":
     uvicorn.run("main:app",host="127.0.0.1", port=8000, reload=True)
