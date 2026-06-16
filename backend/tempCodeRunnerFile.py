17c1e733e9c71e20303ed5b618a9d69c32888944
Siddharth Krishna <siddharthtnj2020@gmail.com>
2026-06-16 07:06:25 +0530

    cleared backend

--- backend/tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-from data_cleaning_bus import df_bus
-from data_cleaning_electric_train import df_rail
-from data_cleaning_metro import df
-print(df_bus.columns)
-print(df_rail.columns)
-print(df.columns)
